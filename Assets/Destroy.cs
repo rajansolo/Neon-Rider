b8abcd67c4c693e27b96543c484d5f5d809e74e5
Rajan Yadav <rj98458@gmail.com>
2022-02-24 19:57:38 +0545

    first commit

---
+++ Assets/Destroy.cs
@@ -0,0 +1,13 @@
+﻿
+using UnityEngine;
+
+public class Destroy : MonoBehaviour {
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.collider.tag == "Player")
+        {
+            Debug.Log("Restart");
+        }
+    }
+}
---
+++ Assets/RiderCollider.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RiderCollider : MonoBehaviour {
+
+    private void OnCollisionEnter2D(Collision2D riderFlip)
+    {
+        if(riderFlip.collider.tag == "FlipCollider")
+        {
+            Debug.Log("You flipped, restart the game!!");
+        }
+    }
+}
